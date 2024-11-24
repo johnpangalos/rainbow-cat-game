0451afc03473a7d806fdff092799b32fc334f13b
John Pangalos <john@pangalos.dev>
2024-11-24 19:13:23 +0100

    move spawn_player to player file

--- src/game/player.rs
+++ src/game/player.rs
@@ -112,3 +112,34 @@ pub fn animate_sprite(
         }
     }
 }
+
+pub fn spawn_player(
+    commands: &mut Commands,
+    asset_server: &Res<AssetServer>,
+    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+) {
+    let texture = asset_server.load("cat-walking.png");
+    let layout = TextureAtlasLayout::from_grid(UVec2::splat(64), 4, 1, None, None);
+    let texture_atlas_layout = texture_atlas_layouts.add(layout);
+    let animation_indices = AnimationIndices { first: 0, last: 3 };
+
+    commands.spawn((
+        SpriteBundle {
+            texture,
+            transform: Transform::from_xyz(0.0, GROUND_HEIGHT, 1.0),
+            ..default()
+        },
+        TextureAtlas {
+            layout: texture_atlas_layout,
+            index: animation_indices.first,
+        },
+        animation_indices,
+        AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
+        Player {
+            speed: PLAYER_SPEED,
+            jump_force: JUMP_FORCE,
+            is_grounded: true,
+        },
+        Velocity(Vec2::ZERO),
+    ));
+}
